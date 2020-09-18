24e683ef67c6731e9402be601c5c7a89428dfdf2
Noor <67762762+Noor12shah@users.noreply.github.com>
2020-09-19 04:02:04 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -29,7 +29,7 @@ async def cmd_list(event):
                         out_file,
                         force_document=True,
                         allow_cache=False,
-                        caption="**COMMANDS** In DARKCOBRA",
+                        caption="**COMMANDS** In FaltuUserBot",
                         reply_to=reply_to_id
                     )
                     await event.delete()
